f29f702fb4c24f060bf12a71d010f80cc991f0f0
ZiF1R <do-alex03@mail.ru>
2021-09-13 18:59:54 +0300

    Complete task 1.a

--- 2 лр/ConsoleApp1/Program.cs
+++ 2 лр/ConsoleApp1/Program.cs
@@ -6,8 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int changes = 132;
-            bool newChanges = true;
+            //* 1. Типы
+
+            // a) Определите переменные всех возможных примитивных типов
+            // С# и проинициализируйте их
+            bool b = true;
+            byte by = 16;
+            char ch = 'a';
+            decimal dec = -123456789;
+            double db = 3.4e-3;
+            float fl = -3.4e38f;
+            int i = 2147483647;
+            long l = -9223372036854775808;
+            sbyte sby = -128;
+            short sh = 32767;
+            uint ui = 0;
+            ulong ul = 18446744073709551615;
+            ushort ush = 65535;
         }
     }
 }

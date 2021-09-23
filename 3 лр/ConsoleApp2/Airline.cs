487b5b0205fd3b95c91fb37e9e5c7f3f3ac28ed0
ZiF1R <do-alex03@mail.ru>
2021-09-23 11:59:21 +0300

    lw3: create class Airline, add constructors and properties

---
+++ 3 лр/ConsoleApp2/Airline.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    // ** Вариант 2 **
+
+    //  Создать класс Airline: Cодержит : Пункт назначения,
+    //  Номер рейса, Тип самолета, Время вылета, Дни недели.
+    //  Свойства и конструкторы должны обеспечивать проверку
+    //  корректности.
+
+    //  Создать массив объектов.Вывести:
+    //  a) список рейсов для заданного пункта назначения;
+    //  b) список рейсов для заданного дня недели;
+
+    class Airline
+    {
+        private string destination;
+        private int flightNumber;
+        private string planeType;
+        private string day;
+        private (int hours, int minutes) departureTime;
+
+        public string Destination
+        {
+            get => destination;
+            set
+            {
+                destination = value;
+            }
+        }
+
+        public int FlightNumber
+        {
+            get => flightNumber;
+            set
+            {
+                if (value.ToString().Length == 6)
+                    flightNumber = value;
+                else
+                    throw new Exception("Номер должен содержать 6 цифр!");
+            }
+        }
+
+        public string PlaneType
+        {
+            get => planeType;
+            set
+            {
+                string[] planeTypes = { "Passenger", "Battle", "Cargo" };
+                if (planeType.Contains(value))
+                    planeType = value;
+                else
+                    throw new Exception("Тип самолета может быть Пассажирский, Боевой или Грузовой!");
+            }
+        }
+
+        public string Day
+        {
+            get => day.ToString();
+            set
+            {
+                string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sanday" };
+
+                if (daysOfWeek.Contains(value))
+                    day = value;
+                else
+                    throw new Exception("Введите корректный день недели!");
+            }
+        }
+
+        public (int hours, int minutes) DepartureTime
+        {
+            get => departureTime;
+            set
+            {
+                if (
+                    (value.hours >= 0 && value.hours < 24) &&
+                    (value.minutes >= 0 && value.minutes < 60)
+                )
+                    departureTime = value;
+                else
+                    throw new Exception("Введите корректное время!");
+            }
+        }
+
+
+        public Airline()
+        {
+            destination = "";
+            flightNumber = 0;
+            planeType = "";
+            day = "";
+            departureTime = (0, 0);
+        }
+
+        public Airline(string destination = "", int flightNumber = 0, string planeType = "", string dayOfWeek = "")
+        {
+            Destination = destination;
+            FlightNumber = flightNumber;
+            PlaneType = planeType;
+            Day = dayOfWeek;
+            DepartureTime = (0, 0);
+        }
+    }
+}
---
+++ 3 лр/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Airline test = new Airline("Minsk", 123456, "Passenger", "12:00:00", "Monday");
+
+            //Console.WriteLine(test.flightNumber);
+            Console.ReadLine();
+        }
+    }
+}

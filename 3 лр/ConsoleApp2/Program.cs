c163126050f8a8f763984b2a83d88361fb2116c9
ZiF1R <do-alex03@mail.ru>
2021-09-25 21:56:08 +0300

    lw3: finish commit

--- 3 лр/ConsoleApp2/Program.cs
+++ 3 лр/ConsoleApp2/Program.cs
@@ -10,18 +10,41 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Airline wqq = new Airline();
-            Airline t = new Airline("Moscow", 654321, "Passenger", "Monday", 12, 12);
-            Airline t1 = new Airline("Tokyo", 123456, "Passenger", "Friday", 3, 00);
-            Airline t2 = new Airline("Berlin", 321642, "Cargo", "Wednesday", 21, 45);
-            Airline t3 = new Airline("USA", 735179, "Cargo", "Monday", 9, 30);
-            Airline[] arr = { t, t1, t2, t3 };
+            Airline tmp1 = new Airline();
+            Airline tmp2 = new Airline("Moscow", 654321, "Passenger", "Monday", 12, 12);
+            Airline tmp3 = new Airline("Tokyo", 123456, "Passenger", "Friday", 3, 00);
+            Airline tmp4 = new Airline("USA", 321642, "Cargo", "Wednesday", 21, 45);
+            Airline tmp5 = new Airline("USA", 735179, "Cargo", "Monday", 9, 30);
+            Airline[] arr = { tmp1, tmp2, tmp3, tmp4, tmp5 };
+
+            Console.WriteLine("Type of Airline-class object: {0}\n" +
+                "Compare: {1}\n" +
+                "Call property / method: tmp2.DepartureTime = {2}; tmp2.GetHashCode() = {3}\n",
+                tmp1.GetType(), tmp4.Equals(tmp5), tmp2.DepartureTime, tmp2.GetHashCode());
+
+            Airline.getInfo();
 
             foreach (Airline plane in Airline.FindByDayOfWeek(ref arr, "Monday"))
                 Console.WriteLine(plane);
-            Console.WriteLine(t.ObjCount);
 
-            Console.ReadLine();
+            foreach (Airline plane in Airline.FindByDestination(ref arr, "USA"))
+                Console.WriteLine(plane);
+
+
+            var anonymousAirline = new
+            {
+                id = 213,
+                destination = "Mexico",
+                flightNumber = 416377,
+                planeType = "Passenger",
+                day = "Tuesday",
+                departureTime = (hours: 7, minutes: 30),
+                departureLocation = "Moscow"
+            };
+
+            Console.WriteLine(anonymousAirline);
+
+            Console.ReadKey();
         }
     }
 }

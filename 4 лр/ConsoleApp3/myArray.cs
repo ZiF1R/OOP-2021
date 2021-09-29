bcc2d2ffcb69de1bd693abb334ee1b7b8f2f4a42
ZiF1R <do-alex03@mail.ru>
2021-09-29 21:54:00 +0300

    lw4: define class myArray with one property and two constructors

---
+++ 4 лр/ConsoleApp3/myArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    //                  Вариант 2
+
+    //  Класс Одномерный массив. Дополнительно перегрузить
+    //  следующие операции: - разность со скалярным значением;
+    //  > проверка на вхождение элемента; != проверка на
+    //  неравенство массивов; + объединение массивов
+
+    //  Методы расширения:
+    //  1) Удаление гласных из строки
+    //  2) Удаление первых пяти элементов
+
+    class myArray
+    {
+        Array array;
+
+        public myArray()
+        {
+            array = null;
+        }
+
+        public myArray(Array arr)
+        {
+            array = arr;
+        }
+    }
+}

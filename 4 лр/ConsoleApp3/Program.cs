52a4bbdfbaf0d555859a394479466a0cc793be5f
ZiF1R <do-alex03@mail.ru>
2021-10-03 10:21:46 +0300

    lw4: add main file which tests facilities of class myArray

---
+++ 4 лр/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myArray arr1 = new myArray(new []{ 1, 2, 3 }, author: "ZiF1R", organisation: "BSTU");
+            myArray arr2 = new myArray(new []{ 3, 2, 1, 3, 2, 1 });
+            Console.WriteLine("> Time of creating arr2: {0}\n", arr1.date.Time);
+
+            Console.WriteLine("> arr1 + arr2 = {0}", arr1 + arr2);
+            Console.WriteLine("> arr1 - arr2 = {0}", arr1 - arr2);
+            Console.WriteLine("> arr1 != arr2 = {0}", arr1 != arr2);
+            Console.WriteLine("> arr1 contains 1 = {0}", arr1 > 1);
+            Console.WriteLine("> Sum of arr1 elements = {0}", arr1.Sum());
+            Console.WriteLine("> Delete vowels from string '{0}' = {1}", "Hello world!", "Hello world!".RemoveVowels());
+            Console.WriteLine("> Remove first five elements from arr2 = {0}", arr2.RemoveFirstFive());
+
+            Console.ReadKey();
+        }
+    }
+}

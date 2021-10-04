8cde40bce8d0aa0c3c966734001c9f3c2000ac92
ZiF1R <do-alex03@mail.ru>
2021-10-04 19:10:49 +0300

    lw5: create start file

---
+++ 5 лр/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+
+        //  Геометрическая фигура, Круг, Прямоугольник,
+        //  Управление (интерфейс с методами show, input, resize и т.д.),
+        //  Элемент управления, Checkbox, Radiobutton, Button.
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}

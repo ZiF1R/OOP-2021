910888ba879c964fcc447f3155ca84c4ccef244d
ZiF1R <do-alex03@mail.ru>
2021-10-06 12:18:37 +0300

    lw5: rename file GeometricFigure to GeometricFigures

---
+++ 5 лр/ConsoleApp4/GeometricFigures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    //  > Геометрическая фигура, Круг, Прямоугольник
+    //  > Управление (интерфейс с методами show, input, resize и т.д.)
+    //  > Элемент управления, Checkbox, Radiobutton, Button
+
+    public class GeometricFigure
+    {
+        public double square;
+        public int height;
+        public int width;
+        public Controls controls;
+    }
+
+    public class Circle : GeometricFigure, IManagement
+    {
+
+        public void show()
+        {
+
+        }
+
+        public void resize()
+        {
+
+        }
+
+        public void input()
+        {
+
+        }
+    }
+}

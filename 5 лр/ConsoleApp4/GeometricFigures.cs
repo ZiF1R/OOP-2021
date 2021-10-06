64d47030039d2d48a40041381dd9d0610177d099
ZiF1R <do-alex03@mail.ru>
2021-10-06 12:57:16 +0300

    lw5: add constructor to class Circle based on class GeometricFigure

--- 5 лр/ConsoleApp4/GeometricFigures.cs
+++ 5 лр/ConsoleApp4/GeometricFigures.cs
@@ -12,14 +12,49 @@ namespace ConsoleApp4
 
     public class GeometricFigure
     {
-        public double square;
-        public int height;
-        public int width;
+        public double height;
+        public double width;
         public Controls controls;
+
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value >= 0) height = value;
+                else throw new Exception("Height can't be less than 0!");
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value >= 0) width = value;
+                else throw new Exception("Width can't be less than 0!");
+            }
+        }
+
+        public GeometricFigure(double height = 0, double width = 0)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
     }
 
     public class Circle : GeometricFigure, IManagement
     {
+        public double square;
+        public double circumference;
+        public double radius;
+
+        public Circle(double height) : base(height)
+        {
+            this.radius = height / 2.0;
+            this.square = Math.PI * Math.Pow(radius, 2);
+            this.circumference = 2 * Math.PI * radius;
+        }
 
         public void show()
         {

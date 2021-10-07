d271e7f484d6a7578e4aff3043552f4777270e9a
ZiF1R <do-alex03@mail.ru>
2021-10-07 11:55:35 +0300

    lw5: move class Circle to independent file; create folder Figures

--- 5 лр/ConsoleApp4/Figures/Circle.cs
+++ 5 лр/ConsoleApp4/Figures/Circle.cs
@@ -4,45 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp4.Figures
 {
-    //  > Геометрическая фигура, Круг, Прямоугольник
-    //  > Управление (интерфейс с методами show, input, resize и т.д.)
-    //  > Элемент управления, Checkbox, Radiobutton, Button
-
-    public class GeometricFigure
-    {
-        public double height;
-        public double width;
-        public Controls controls;
-
-        public double Height
-        {
-            get => height;
-            set
-            {
-                if (value >= 0) height = value;
-                else throw new Exception("Height can't be less than 0!");
-            }
-        }
-
-        public double Width
-        {
-            get => width;
-            set
-            {
-                if (value >= 0) width = value;
-                else throw new Exception("Width can't be less than 0!");
-            }
-        }
-
-        public GeometricFigure(double height = 0, double width = 0)
-        {
-            this.Height = height;
-            this.Width = width;
-        }
-    }
-
     public class Circle : GeometricFigure, IManagement
     {
         public double square;
@@ -58,12 +21,12 @@ namespace ConsoleApp4
 
         public void show()
         {
-            for (int i = 1; i < (int)radius*2 + 1; i += 2)
+            for (int i = 1; i < (int)radius * 2 + 1; i += 2)
             {
                 int strWidth = (int)Math.Ceiling((double)((int)radius * 2 - i) / 2.0);
                 Console.WriteLine("".PadLeft(strWidth, ' ').PadRight(strWidth + i, '0'));
             }
-            for (int i = (int)radius*2 + 1; i > 0; i -= 2)
+            for (int i = (int)radius * 2 + 1; i > 0; i -= 2)
             {
                 int strWidth = (int)Math.Ceiling((double)((int)radius * 2 - i) / 2.0);
                 Console.WriteLine("".PadLeft(strWidth, ' ').PadRight(strWidth + i, '0'));
---
+++ 5 лр/ConsoleApp4/GeometricFigure.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    //  > Геометрическая фигура, Круг, Прямоугольник
+    //  > Управление (интерфейс с методами show, input, resize и т.д.)
+    //  > Элемент управления, Checkbox, Radiobutton, Button
+
+    public class GeometricFigure
+    {
+        public double height;
+        public double width;
+        public Controls controls;
+
+        public double Height
+        {
+            get => height;
+            set
+            {
+                if (value >= 0) height = value;
+                else throw new Exception("Height can't be less than 0!");
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            set
+            {
+                if (value >= 0) width = value;
+                else throw new Exception("Width can't be less than 0!");
+            }
+        }
+
+        public GeometricFigure(double height = 0, double width = 0)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+    }
+}

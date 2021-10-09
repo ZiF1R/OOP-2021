a86fd97249b78550cb3824ac59752046b68dcfec
ZiF1R <do-alex03@mail.ru>
2021-10-09 18:39:37 +0300

    lw5: move all interfaces to independent folder

---
+++ 5 лр/ConsoleApp4/Interfaces/IManagement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    interface IManagement
+    {
+        void show();
+        void resize(double height = 0, double width = 0);
+        void input();
+    }
+}

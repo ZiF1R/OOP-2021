2aa2ed650fb17fb6afb42d40fb17c3e4c36f4e46
ZiF1R <do-alex03@mail.ru>
2021-10-10 14:27:58 +0300

    lw5: add class Printer

---
+++ 5 лр/ConsoleApp4/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public static class Printer
+    {
+        public static void IAmPrinting(GeometricFigure figure)
+        {
+            Console.WriteLine(figure.ToString());
+        }
+    }
+}

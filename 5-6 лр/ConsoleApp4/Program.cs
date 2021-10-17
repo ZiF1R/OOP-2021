328f3090783b174d11bb53af9ccee16ba425ec6c
ZiF1R <do-alex03@mail.ru>
2021-10-17 08:41:37 +0300

    Change folder name from '5 лр' to '5-6 лр'

---
+++ 5-6 лр/ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp4.Figures;
+using ConsoleApp4.Controls;
+using ConsoleApp4.Container;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(13, ConsoleColor.Green);
+            Circle circle2 = new Circle(11, ConsoleColor.DarkMagenta);
+            Rectangle rect = new Rectangle(5, 2, ConsoleColor.Cyan);
+            Button btn1 = new Button(true, "btn1");
+            Button btn2 = new Button(false, "btn2");
+
+            //Console.WriteLine(ui[0]);
+
+            //ui.ShowAllButtons();
+            //Console.WriteLine(ui.GetLength());
+            //ui.GetTotalFiguresSquare();
+
+            UI ui = new UI(circle1, btn1, circle2);
+
+            Console.WriteLine(UIController.GetTotalFiguresSquare(ui));
+
+            //ui.Show();
+            //ui.Remove(1);
+            //Console.WriteLine();
+            //ui.Show();
+
+            Console.ReadKey();
+        }
+    }
+}

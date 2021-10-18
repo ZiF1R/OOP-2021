46978215d822d5c9a50a92d79611e59052a45764
ZiF1R <do-alex03@mail.ru>
2021-10-18 12:52:08 +0300

    lw6: add code which tests facilities of created classes

--- 5-6 лр/ConsoleApp4/Program.cs
+++ 5-6 лр/ConsoleApp4/Program.cs
@@ -14,25 +14,26 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Circle circle1 = new Circle(13, ConsoleColor.Green);
-            Circle circle2 = new Circle(11, ConsoleColor.DarkMagenta);
+            Circle circle2 = new Circle(11, ConsoleColor.Yellow);
             Rectangle rect = new Rectangle(5, 2, ConsoleColor.Cyan);
             Button btn1 = new Button(true, "btn1");
             Button btn2 = new Button(false, "btn2");
 
-            //Console.WriteLine(ui[0]);
-
-            //ui.ShowAllButtons();
-            //Console.WriteLine(ui.GetLength());
-            //ui.GetTotalFiguresSquare();
+            circle1.center.getCoordinates();
 
             UI ui = new UI(circle1, btn1, circle2);
+            Console.WriteLine("\n> Get element at index:\n{0}\n", ui[1]);
+            ui.Add(rect);
+            ui.Add(btn2);
+            ui.Remove(0);
 
-            Console.WriteLine(UIController.GetTotalFiguresSquare(ui));
+            Console.WriteLine("\n> All figures:\n");
+            ui.Show();
 
-            //ui.Show();
-            //ui.Remove(1);
-            //Console.WriteLine();
-            //ui.Show();
+            Console.WriteLine("\n> Get total figures square: {0}", UIController.GetTotalFiguresSquare(ui));
+            Console.WriteLine("\n> Show all buttons in ui:\n");
+            UIController.ShowAllButtons(ui);
+            Console.WriteLine("\n> Get length of ui: {0}", UIController.GetLength(ui));
 
             Console.ReadKey();
         }

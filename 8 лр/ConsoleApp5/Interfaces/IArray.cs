bd3101638974e8a247e165944d3e3d63256bb04a
ZiF1R <do-alex03@mail.ru>
2021-10-21 09:33:48 +0300

    lw8: add interface IArray

---
+++ 8 лр/ConsoleApp5/Interfaces/IArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Interfaces
+{
+    interface IArray<T>
+    {
+        T Add(T elem);
+        T Remove(T elem);
+        void Show();
+    }
+}

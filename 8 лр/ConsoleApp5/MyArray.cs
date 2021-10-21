347ca4e021d69af135447715fd6916cc7d71b975
ZiF1R <do-alex03@mail.ru>
2021-10-21 10:04:28 +0300

    lw8: add class MyArray

---
+++ 8 лр/ConsoleApp5/MyArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp5.Interfaces;
+
+namespace ConsoleApp5
+{
+    public partial class MyArray<T> : IArray<T>
+    {
+        public T[] array;
+        public readonly Date date;
+        public readonly Owner owner;
+
+        // subclasses
+
+        public class Owner
+        {
+            public int ID { get; }
+            public string Author { get; }
+            public string Organization { get; }
+
+            public Owner(int id, string author, string organization)
+            {
+                this.ID = id;
+                this.Author = author;
+                this.Organization = organization;
+            }
+        }
+        public class Date
+        {
+            public DateTime Time { get; }
+
+            public Date()
+            {
+                Time = DateTime.Now;
+            }
+        }
+
+        // constructors
+
+        public MyArray()
+        {
+            this.array = null;
+            this.date = new Date();
+            this.owner = new Owner(0, "", "");
+        }
+
+        public MyArray(T[] arr, int id = 0, string author = "", string organisation = "")
+        {
+            array = arr;
+            this.date = new Date();
+            this.owner = new Owner(id, author, organisation);
+        }
+    }
+}

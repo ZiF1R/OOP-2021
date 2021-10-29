cd6838689d236efb186f5d872178ffee178330ef
ZiF1R <do-alex03@mail.ru>
2021-10-29 17:02:31 +0300

    lw10: add class Student to project

---
+++ 10 лр/ConsoleApp7/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Student
+    {
+        public string name;
+        private uint course;
+
+        public uint Course
+        {
+            get => course;
+            set
+            {
+                if (value > 0) this.course = value;
+                else this.course = 1;
+            }
+        }
+
+        public Student(string name, uint course = 1)
+        {
+            this.name = name;
+            this.Course = course;
+        }
+
+        public override string ToString()
+        {
+            return $"\nStudent: {this.name}\n" +
+                $"Course: {this.course}\n";
+        }
+    }
+}

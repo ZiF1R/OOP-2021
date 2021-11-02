9a156cb64679c414f7649f86f2a59df132d01280
ZiF1R <do-alex03@mail.ru>
2021-11-02 08:32:29 +0300

    lw11: complete 3rd task

--- 11 лр/ConsoleApp8/Program.cs
+++ 11 лр/ConsoleApp8/Program.cs
@@ -80,13 +80,89 @@ namespace ConsoleApp8
             {
                 new myArray(new []{ 9, 2, 3, 4 }),
                 new myArray(new []{ 4, 5, 1, 2 }),
-                new myArray(new []{ 7, 6, 1, 4 })
+                new myArray(new []{ 1, 5, 8, 2 }),
+                new myArray(new []{ 0, 3, 7, 2 }),
+                new myArray(new []{ 12, 1, 2, 7 }),
+                new myArray(new []{ 8 }),
+                new myArray(new []{ 4 }),
+                new myArray(new []{ -4, 5, -8, 2 }),
+                new myArray(new []{ 0, -7, -3, -1 }),
+                new myArray(new []{ 4, -5, 9, -1 }),
+                new myArray(new []{ 7, 6, 1 }),
+                new myArray(new []{ 7, 6, 1, 4 }),
+                new myArray(new []{ 4, -5, 9 })
             };
+
 
             //3.На основе LINQ сформируйте следующие запросы по вариантам.
             //При необходимости добавьте в класс T(тип параметра) свойства.
 
-           Console.ReadKey();
+            // queries:
+            //  стек с наименьшим / наибольшим верхним элементом;
+            Console.WriteLine("\n> Arrays where first element is maximal or minimal:\n");
+            var query1 =
+                from array in arrays
+                where (array.Max() == array[0] || array.Min() == array[0])
+                select array;
+
+            foreach (myArray array in query1)
+                Console.WriteLine(array);
+
+
+            //  список стеков, содержащих отрицательные элементы.
+            Console.WriteLine("\n> Arrays with negative numbers:\n");
+            var query2 =
+                from array in arrays
+                where (array.Where((el) => el < 0).Length > 0)
+                select array;
+
+            foreach (myArray array in query2)
+                Console.WriteLine(array);
+
+
+            //  Минимальный стек
+            Console.WriteLine("\n> Minimal array:\n");
+            var query3 =
+                from array in arrays
+                orderby (array.Sum())
+                select array;
+
+            Console.WriteLine(query3.First());
+
+
+            //  Массив стеков длины 1 и 3
+            Console.WriteLine("\n> Arrays where length is 1 or 3:\n");
+            var query4 =
+                from array in arrays
+                where (array.Length() == 1 || array.Length() == 3)
+                select array;
+
+            foreach (myArray array in query4)
+                Console.WriteLine(array);
+
+
+            //  Первый стек с нулевым элементом
+            Console.WriteLine("\n> First array with zero element:\n");
+            var query5 =
+                from array in arrays
+                where (array.Contains(0))
+                select array;
+
+            Console.WriteLine(query5.First());
+
+
+            //  Упорядоченный массив стеков по сумме элементов
+            Console.WriteLine("\n> Order by sum:\n");
+            var query6 =
+                from array in arrays
+                orderby (array.Sum())
+                select array;
+
+            foreach (myArray array in query6)
+                Console.WriteLine(array);
+
+
+            Console.ReadKey();
         }
     }
 }

4bb74cf16a10692fb6d4174ccd0019ecc5bb2510
ZiF1R <do-alex03@mail.ru>
2022-01-24 13:25:40 +0300

    lw1: Add class Square to project

---
+++ 2 semester/1 lw/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_lw
+{
+    public class Square
+    {
+        public int EdgeSize { get; set; }
+
+        public Square(int edgeSize = 0)
+        {
+            EdgeSize = edgeSize;
+        }
+
+        public override string ToString()
+        {
+            return $"Size of square: {this.EdgeSize}×{this.EdgeSize}";
+        }
+    }
+}

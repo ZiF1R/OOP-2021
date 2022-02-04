349aa969c5205da1c9899c5787f101810df099d1
ZiF1R <do-alex03@mail.ru>
2022-02-04 14:53:00 +0300

    lw4-6: Add recently opened files history

--- 2 semester/4-6 lw/MainWindow.xaml.cs
+++ 2 semester/4-6 lw/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -23,10 +24,14 @@ namespace test
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] recentFilesHistory = { };
+        bool isChangesSaved = true;
+
         public MainWindow()
         {
             InitializeComponent();
             this.SetDefaultEditorStyles();
+            this.FindRecentOpenedFiles();
             window.Title += " ・ " + Directory.GetCurrentDirectory();
         }
 
@@ -38,6 +43,27 @@ namespace test
             WorkField.FontSize = Convert.ToDouble(
                 Regex.Replace(FontSize.SelectedItem.ToString().Trim(), @".*:\s+", "")
             );
+        }
+
+        private void FindRecentOpenedFiles()
+        {
+            if (File.Exists("recentFiles.json"))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(string[]));
+                using (FileStream fs = new FileStream("recentFiles.json", FileMode.Open))
+                {
+                    string[] restoredFilesHistory = (string[])serializer.ReadObject(fs);
+                    if (restoredFilesHistory != null)
+                        this.recentFilesHistory = restoredFilesHistory;
+                }
+                foreach (string path in this.recentFilesHistory)
+                {
+                    MenuItem menuItem = new MenuItem();
+                    menuItem.Header = path;
+                    menuItem.Click += this.OpenRecent_Click;
+                    OpenedFilesList.Items.Add(menuItem);
+                }
+            }
         }
 
         /// 
@@ -152,6 +178,7 @@ namespace test
 
         private void WorkField_TextChanged(object sender, TextChangedEventArgs e)
         {
+            this.isChangesSaved = false;
             TextRange text = new TextRange(WorkField.Document.ContentStart, WorkField.Document.ContentEnd);
             int charactersNumber = Regex.Replace(text.Text, @"[\s\n\v\f\r]", "").Length;
             if (CharactersStatus != null)
@@ -163,18 +190,18 @@ namespace test
         /// 
         private void New_Click(object sender, RoutedEventArgs e)
         {
-            var answer = MessageBox.Show(
-                "Are you sure? After this action all changes will disappear!",
-                "Warning!",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Warning
-            );
-
-            if (answer == MessageBoxResult.OK)
+            if (!this.isChangesSaved)
             {
-                WorkField.Document.Blocks.Clear();
-                window.Title = "New file ・ " + Directory.GetCurrentDirectory();
+                var answer = MessageBox.Show(
+                    "Are you sure? After this action all unsaved changes will disappear!",
+                    "Warning!",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning
+                );
+                if (answer == MessageBoxResult.Cancel) return;
             }
+            WorkField.Document.Blocks.Clear();
+            window.Title = "New file ・ " + Directory.GetCurrentDirectory();
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
@@ -201,61 +228,119 @@ namespace test
                         return;
                     }
                 }
-                window.Title = this.GetFileName(file) + " ・ " + Directory.GetCurrentDirectory();
+                window.Title = this.GetFileName(file.FileName) + " ・ " + Directory.GetCurrentDirectory();
+                this.SaveToRecentFilesHistory(file.FileName);
+                this.isChangesSaved = true;
             }
         }
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            var answer = MessageBox.Show(
-                "Are you sure? After this action all changes will disappear!",
-                "Warning!",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Warning
-            );
-            if (answer == MessageBoxResult.Cancel) return;
+            if (!this.isChangesSaved)
+            {
+                var answer = MessageBox.Show(
+                    "Are you sure? After this action all unsaved changes will disappear!",
+                    "Warning!",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning
+                );
+                if (answer == MessageBoxResult.Cancel) return;
+            }
 
-            TextRange text = new TextRange(WorkField.Document.ContentStart, WorkField.Document.ContentEnd);
             OpenFileDialog file = new OpenFileDialog();
             file.Filter = "All files (*.*)|*.*|Text File (*.txt)|*.txt|Rich Text Format (*.rtf)|*.rtf";
 
             if (file.ShowDialog() == true)
-            {
-                FileStream fs = new FileStream(file.FileName, FileMode.Open);
-                TextRange range = new TextRange(WorkField.Document.ContentStart, WorkField.Document.ContentEnd);
-                string extension = System.IO.Path.GetExtension(file.FileName).ToLower();
+                this.ReadFromFile(file.FileName);
 
-                if (extension != ".rtf" && extension != ".txt")
-                {
-                    MessageBox.Show(
-                        "Wrong file extension!",
-                        "Error",
-                        MessageBoxButton.OK,
-                        MessageBoxImage.Error
-                    );
-                    return;
-                }
-                range.Load(fs, DataFormats.Rtf);
-                window.Title = this.GetFileName(file) + " ・ " + Directory.GetCurrentDirectory();
+            this.SaveToRecentFilesHistory(file.FileName);
+        }
+
+        private void OpenRecent_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            string filePath = menuItem.Header.ToString();
+            if (menuItem != null && filePath != null)
+            {
+                this.ReadFromFile(filePath);
+                this.SaveToRecentFilesHistory(filePath);
             }
         }
 
-        private string GetFileName(SaveFileDialog file)
+        private string GetFileName(string fullPath)
         {
-            string fileName = Regex.Match(file.FileName, @"\\[^\\]+\..+$").Value;
+            string fileName = Regex.Match(fullPath, @"\\[^\\]+\..+$").Value;
             fileName = Regex.Replace(fileName, @"\\", "");
             //fileName = Regex.Replace(fileName, @"\..+", "");
 
             return fileName;
         }
 
-        private string GetFileName(OpenFileDialog file)
+        private void SaveToRecentFilesHistory(string newPath)
         {
-            string fileName = Regex.Match(file.FileName, @"\\[^\\]+\..+$").Value;
-            fileName = Regex.Replace(fileName, @"\\", "");
-            //fileName = Regex.Replace(fileName, @"\..+", "");
+            // behavior:
 
-            return fileName;
+            // if function received path which already exist in this.recentFilesHistory
+                // => bubble it to first position
+
+            // otherwise
+                // if this.recentFilesHistory already have 10 files paths
+                    // => remove the last one and add new item to the head
+
+                // else add new item to the head
+            // at the end save new history to file <recentFiles.json>
+
+            if (this.recentFilesHistory.Contains(newPath))
+            {
+                this.recentFilesHistory = this.recentFilesHistory.Where(path => path != newPath).Prepend(newPath).ToArray();
+            }
+            else
+            {
+                if (this.recentFilesHistory.Length == 10)
+                {
+                    this.recentFilesHistory = this.recentFilesHistory
+                        .Where((path, index) => index != this.recentFilesHistory.Length - 1)
+                        .Prepend(newPath)
+                        .ToArray();
+                }
+                else
+                    this.recentFilesHistory = this.recentFilesHistory.Prepend(newPath).ToArray();
+            }
+            OpenedFilesList.Items.Clear();
+            foreach (string path in this.recentFilesHistory)
+            {
+                MenuItem menuItem = new MenuItem();
+                menuItem.Header = path;
+                menuItem.Click += this.OpenRecent_Click;
+                OpenedFilesList.Items.Add(menuItem);
+            }
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(string[]));
+            using (FileStream fs = new FileStream("recentFiles.json", FileMode.Create))
+            {
+                serializer.WriteObject(fs, this.recentFilesHistory);
+            }
+        }
+
+        private void ReadFromFile(string fullPath)
+        {
+            FileStream fs = new FileStream(fullPath, FileMode.Open);
+            TextRange range = new TextRange(WorkField.Document.ContentStart, WorkField.Document.ContentEnd);
+            string extension = System.IO.Path.GetExtension(fullPath).ToLower();
+
+            if (extension != ".rtf" && extension != ".txt")
+            {
+                MessageBox.Show(
+                    "Wrong file extension!",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error
+                );
+                return;
+            }
+            range.Load(fs, DataFormats.Rtf);
+            window.Title = this.GetFileName(fullPath) + " ・ " + Directory.GetCurrentDirectory();
+            this.isChangesSaved = true;
         }
 
         /// 
@@ -281,16 +366,19 @@ namespace test
         /// 
         private void window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            var answer = MessageBox.Show(
-                "Are you sure? After this action all changes will disappear!",
-                "Warning!",
-                MessageBoxButton.OKCancel,
-                MessageBoxImage.Warning
-            );
+            if (!this.isChangesSaved)
+            {
+                var answer = MessageBox.Show(
+                    "Are you sure? After this action all unsaved changes will disappear!",
+                    "Warning!",
+                    MessageBoxButton.OKCancel,
+                    MessageBoxImage.Warning
+                );
 
-            if (answer == MessageBoxResult.Cancel)
-                e.Cancel = true;
-            else e.Cancel = false;
+                if (answer == MessageBoxResult.Cancel)
+                    e.Cancel = true;
+                else e.Cancel = false;
+            }
         }
     }
 }

845e3e9b9dcd335b9ff857c26ffab320919f36f2
ZiF1R <do-alex03@mail.ru>
2022-03-08 14:35:38 +0300

    Add CustomInput component

---
+++ 2 semester/4-6 lw/components/CustomInput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace test.components
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:test.components"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он 
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:test.components;assembly=test.components"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///
+    ///
+    /// Шаг 2)
+    /// Теперь можно использовать элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:CustomInput/>
+    ///
+    /// </summary>
+    public class CustomInput : Control
+    {
+        static CustomInput()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomInput), new FrameworkPropertyMetadata(typeof(CustomInput)));
+        }
+    }
+}

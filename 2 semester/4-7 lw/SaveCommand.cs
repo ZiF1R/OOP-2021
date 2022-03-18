2a903f07ea0c47f80216f2d9bf10e51006b0331e
ZiF1R <do-alex03@mail.ru>
2022-03-18 17:20:31 +0300

    lw4-7: Add SaveCommand

---
+++ 2 semester/4-7 lw/SaveCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace test
+{
+    public class SaveCommand
+    {
+        public static RoutedCommand Save { get; set; }
+
+        static SaveCommand()
+        {
+            Save = new RoutedCommand("Save", typeof(MainWindow));
+        }
+    }
+}
